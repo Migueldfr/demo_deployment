0fd64e3412b4cafad70af1b9303346b388cb57c2
Migueldfr <116830299+Migueldfr@users.noreply.github.com>
2023-03-03 10:52:46 +0100

    loadcommit

--- app_model.py
+++ app_model.py
@@ -89,7 +89,7 @@ def retrain():
 
             scores = cross_val_score(model, X,y, cv=10 , scoring = 'neg_mean_absolute_error')
 
-        return f"The model has beend re-trained, it is now ready to break the rules 🤟🏽" #+ ' The MAE now is: ' + str(round(scores.mean()*(-1),2))
+        return f"The model has beend re-trained, it is now ready to break the rules 🤟🏽" + ' The MAE now is: ' + str(round(scores.mean()*(-1),2))
 
 
-#app.run()    
+app.run()    
